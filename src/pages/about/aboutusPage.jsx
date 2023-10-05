ae16d71b7bac40ab15e452749c9746c44fe2b80d
Senadheera330 <101629162+Senadheera330@users.noreply.github.com>
2023-10-05 21:32:47 +0530

    v.1.0.0

--- src/pages/about/aboutusPage.jsx
+++ src/pages/about/aboutusPage.jsx
@@ -1,10 +1,49 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import banner from "../../assets/img/AboutBanner.png";
+import { mainWidth, white } from "../../config";
 
 function AboutusPage() {
     return (
         <Box>
-        <h1>about</h1>
+        {/* Banner Section */}
+        <Box
+            sx={{
+            background: `url(${banner})`,
+            height: "40vh",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            display: "flex",
+            }}
+        >
+            <Box
+            sx={{
+                width: mainWidth,
+                marginX: "auto",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-end",
+                alignItems: "center",
+            }}
+            >
+            <Box
+                sx={{
+                backgroundColor: white,
+                padding: "3rem 9rem",
+                borderTopLeftRadius: "2rem",
+                borderTopRightRadius: "2rem",
+                }}
+            >
+                <Typography
+                variant="h3"
+                color="initial"
+                fontFamily={"DM Serif Display"}
+                >
+                About Us
+                </Typography>
+            </Box>
+            </Box>
+        </Box>
         </Box>
     );
 }
